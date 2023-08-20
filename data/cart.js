a20bb24816e5daff0d2f006cdad8ee7446acf3fe
wasimAkram8529 <codelikearocstar@gmail.com>
2023-08-20 14:26:31 +0530

    Move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,19 @@
 export const cart = [];
+export function addToCart(productId){
+  let matchingItem;
+  cart.forEach((item) => {
+      if(productId === item.productId){
+         matchingItem = item;
+      }
+  });
+
+  if(matchingItem){
+    matchingItem.quantity += 1;
+  }
+  else{
+   cart.push({
+     productId: productId,
+     quantity: 1
+    });
+  }
+} 
