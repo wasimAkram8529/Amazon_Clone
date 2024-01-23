ba73d1a127cfb9a6db7d6aedcb4a094e17b7dbee
wasimAkram8529 <codelikearocstar@gmail.com>
2024-01-23 11:04:11 +0530

    Save data and generate html for payment summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,29 +1,22 @@
 import { cart, removeFromCart, updateDeliveryOPtion } from "../../data/cart.js";
-import { products } from "../../data/products.js";
+import { products, getProduct } from "../../data/products.js";
 import { formateCurrency } from "../utils/money.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
-import { deliveryOptions } from "../../data/deliveryOptions.js";
+import {
+  deliveryOptions,
+  getDeliveryOption,
+} from "../../data/deliveryOptions.js";
 
 export function renderOrderSummary() {
   let cartSummaryHTML = "";
   cart.forEach((cartItem) => {
     const productId = cartItem.productId;
 
-    let matchingProduct;
-    products.forEach((product) => {
-      if (product.id === productId) {
-        matchingProduct = product;
-      }
-    });
+    const matchingProduct = getProduct(productId);
 
     const deliveryOptionId = cartItem.deliveryOptionId;
 
-    let deliveryOption;
-    deliveryOptions.forEach((option) => {
-      if (option.id === deliveryOptionId) {
-        deliveryOption = option;
-      }
-    });
+    const deliveryOption = getDeliveryOption(deliveryOptionId);
 
     const today = dayjs();
     const deliveryDate = today.add(deliveryOption.deliveryDays, "days");
--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,3 +1,3 @@
-export function formateCurrency(priceCents){
-  return (priceCents/100).toFixed(2);
+export function formateCurrency(priceCents) {
+  return (Math.round(priceCents) / 100).toFixed(2);
 }
